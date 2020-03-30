36edbe04b2fbbc1e2e0f8e73f66ceeaa42bbc1d7
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-30 12:46:14 -0300

    visualize problems page styling

--- src/pages/VisualizeProblems/index.js
+++ src/pages/VisualizeProblems/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text, TouchableOpacity, StatusBar } from 'react-native';
+import { TouchableOpacity, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Background from '~/components/Background';
 
-import { Container, Strip } from './styles';
+import {
+  Container,
+  Strip,
+  Product,
+  Problems,
+  Problem,
+  Occurence,
+  OccurenceDate,
+} from './styles';
 
 export default function VisualizeProblems() {
   return (
@@ -12,14 +20,24 @@ export default function VisualizeProblems() {
       <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
       <Strip />
       <Container>
-        <Text>Notify</Text>
+        <Product>Produto</Product>
+        <Problems>
+          <Problem>
+            <Occurence>Destinatário ausente</Occurence>
+            <OccurenceDate>14/01/2020</OccurenceDate>
+          </Problem>
+          <Problem>
+            <Occurence>Destinatário ausente</Occurence>
+            <OccurenceDate>15/01/2020</OccurenceDate>
+          </Problem>
+        </Problems>
       </Container>
     </Background>
   );
 }
 
 VisualizeProblems.navigationOptions = ({ navigation }) => ({
-  title: 'Confirmar entrega',
+  title: 'Visualizar problemas',
   headerLeft: () => (
     <TouchableOpacity onPress={() => navigation.goBack()}>
       <Icon name="chevron-left" size={20} color="#fff" />
--- src/pages/VisualizeProblems/styles.js
+++ src/pages/VisualizeProblems/styles.js
@@ -1,8 +1,42 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 0 20px;
+  margin-top: -75px;
+`;
 
 export const Strip = styled.SafeAreaView`
   height: 100px;
   background: #7d40e7;
+`;
+
+export const Product = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 15px;
+`;
+
+export const Problems = styled.View``;
+
+export const Problem = styled.View`
+  background: #fff;
+  border: 1px solid #0000001a;
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding: 17px 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Occurence = styled.Text`
+  font-size: 16px;
+  color: #999;
+`;
+
+export const OccurenceDate = styled.Text`
+  font-size: 12px;
+  color: #c1c1c1;
 `;
