4f053b048160c7530ea56b0fa1bfdae18cda9698
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-30 19:12:23 -0300

    confirm delivery page styling camera integration

--- src/pages/ConfirmDelivery/index.js
+++ src/pages/ConfirmDelivery/index.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import { Text, TouchableOpacity, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { RNCamera } from 'react-native-camera';
 
 import Background from '~/components/Background';
 
-import { Container, Strip } from './styles';
+import {
+  Container,
+  Strip,
+  Signature,
+  Camera,
+  TakePicture,
+  Preview,
+  Snap,
+  SubmitButton,
+  SubmitButtonText,
+} from './styles';
 
-export default function ConfirmProblem() {
+export default function ConfirmDelivery() {
+  let camera;
+
+  const [preview, setPreview] = useState('');
+
+  async function handleTakePicture() {
+    if (camera) {
+      // const options = { quality: 0.5, base64: true };
+      const data = await camera.takePictureAsync();
+      console.tron.log(data);
+      setPreview(data.uri);
+    }
+  }
+
+  function handleConfirmDelivery() {}
+
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
       <Strip />
       <Container>
-        <Text>Confirm</Text>
+        <Signature>
+          {!preview ? (
+            <>
+              <Camera
+                ref={(ref) => {
+                  camera = ref;
+                }}
+                captureAudio={false}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.auto}
+                autoFocus={RNCamera.Constants.AutoFocus.on}
+                androidCameraPermissionOptions={{
+                  title: 'Permission to use camera',
+                  message: 'We need your permission to use your camera',
+                  buttonPositive: 'Ok',
+                  buttonNegative: 'Cancel',
+                }}
+              />
+              <TakePicture>
+                <Snap onPress={handleTakePicture}>
+                  <Icon name="camera" size={40} color="#fff" />
+                </Snap>
+              </TakePicture>
+            </>
+          ) : (
+            <Preview
+              source={{
+                uri: preview,
+              }}
+            />
+          )}
+        </Signature>
+        <SubmitButton onPress={handleConfirmDelivery}>
+          <SubmitButtonText>Enviar</SubmitButtonText>
+        </SubmitButton>
       </Container>
     </Background>
   );
 }
 
-ConfirmProblem.navigationOptions = ({ navigation }) => ({
+ConfirmDelivery.navigationOptions = ({ navigation }) => ({
   title: 'Confirmar entrega',
   headerLeft: () => (
     <TouchableOpacity onPress={() => navigation.goBack()}>
--- src/pages/ConfirmDelivery/styles.js
+++ src/pages/ConfirmDelivery/styles.js
@@ -1,8 +1,62 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import { RNCamera } from 'react-native-camera';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 0 20px;
+  margin-top: -75px;
+`;
 
 export const Strip = styled.SafeAreaView`
   height: 100px;
   background: #7d40e7;
+`;
+
+export const Signature = styled.View`
+  border-radius: 4px;
+  background: #000;
+  height: 495px;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Camera = styled(RNCamera)`
+  margin-top: 197px;
+  height: 100px;
+  width: 372px;
+  border-radius: 4px;
+`;
+
+export const TakePicture = styled.View`
+  align-items: center;
+  padding: 20px 0;
+`;
+
+export const Preview = styled.Image`
+  flex: 1;
+`;
+
+export const Snap = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: #0000004d;
+  border-radius: 40px;
+  width: 80px;
+  height: 80px;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  height: 45px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  border-radius: 4px;
+  background: #7d40e7;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
 `;
